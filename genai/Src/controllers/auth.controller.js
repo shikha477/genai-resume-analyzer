cb24a89576cbbdc6cecab3966f2e4616707c3ac3
shikha477 <shikha479saini@gmail.com>
2026-06-06 10:58:02 +0530

    creat login register

--- genai/Src/controllers/auth.controller.js
+++ genai/Src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const userModel = require('../models/user.model');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const tokenBlacklistModel = require('../models/blacklist.model');
 
 /**
  * @name registerUserController
@@ -20,9 +22,76 @@ async function registerUserController(req,res){
         return res.status(400).json({
             message:"Account already exists with this username or email address"})
     }
+    const hash = await bcrypt.hash(password,10);
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash
+    })
+    const token = jwt.sign (
+        {
+            id: user.id, username: user.usernme
+        },
+        process.env.JWT_SECRET,
+        {expiresIn: '1d'}
+    )
+
+    res.cookie('token',token)
+    res.status(201).json({
+        message:"User registered successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
 }
 
+/**
+ * @name loginUserController
+ * @description login a user. expects email and password in  the reques body 
+ * @access Public
+ */
+
+async  function loginUserController(req,res){
+    const {email,password} = req.body
+    const user = await userModel.findOne({email})
+    if (!user){
+        return res.status(400).json({message:"Invalid email or password"})
+    }
+    const isPasswordValid = await bcrypt.compare(password,user.password)
+    if (!isPasswordValid){
+        return res.status(400).json({message:"Invalid email or password"})
+    }
+    const token = jwt.sign (
+        {
+            id: user.id, username: user.usernme
+        },
+        process.env.JWT_SECRET,
+        {expiresIn: '1d'}
+    )
+    res.cookie('token',token)
+    res.status(200).json({
+        message:"User logged in successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+async function logoutUserController(req,res){
+    const token = req.cookies.token;
+    if (token){
+        await tokenBlacklistModel.create({token});
+    }
+    res.clearCookie('token');
+    res.status(200).json({message:"User logged out successfully"})
+}
 
 module.exports = {
-    registerUserController
+    registerUserController,
+    loginUserController,
+    logoutUserController
 }
--- genai/Src/routes/auth.routes.js
+++ genai/Src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const authController = require('../controllers/auth.controller');
 
+
 const authRouter =Router();
 
 /**
@@ -9,5 +10,20 @@ const authRouter =Router();
  * @access Public
  */
 authRouter.post("/register",authController.registerUserController)
+
+/**
+ * @route POST /api/auth/login
+ * @description login user with email and password
+ * @access Public
+ */
+
+authRouter.post("/login",authController.loginUserController)
+
+/**
+ * @route GET/api/auth/logout
+ * @description clear token from user cookie and add the token in blacklist
+ * @access Public
+ */
+authRouter.get("/logout",authController.logoutUserController)
 
 module.exports = authRouter;
